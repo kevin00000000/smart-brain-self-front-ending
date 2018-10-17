8eee211b210ce1f7b47f8221eb887077fb2b0ac2
Kevin00000000 <jiannan_yang@163.com>
2018-10-17 16:38:53 +0800

    to heroku smart-brain-self

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -21,7 +21,7 @@ class Signin extends React.Component {
     }
 
     onSubmit = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://smart-brain-self.herokuapp.com/signin', {
             method: 'post',
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
